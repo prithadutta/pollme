daf4c2109512041e3e1287f674786adf2b50aa7a
prithadutta <pd1057@wildcats.unh.edu>
2018-02-26 12:48:12 -0500

    updating serializers.py, views.py and settings.py

--- src/pollme/polls/api/serializers.py
+++ src/pollme/polls/api/serializers.py
@@ -16,10 +16,20 @@ class QuestionListSerializer(ModelSerializer):
     Reference this stack overflow for the choices:
         https://stackoverflow.com/questions/33945148/return-nested-serializer-in-serializer-method-field
     """
-    pass
+    choices = SerializerMethodField(source='get_choices')
+    class Meta:
+        model = Question
+        fields = ('id', 'text', 'pub_date', 'choices')
+
+    def get_choices(self, obj):
+        return ChoiceSerializer(obj.choice_set.all(), many=True).data
+    
 
 class ChoiceSerializer(ModelSerializer):
     """
     This serializes the Choice model
     """
-    pass
+    class Meta:
+        model = Choice
+        fields = ('choice_text', 'votes')
+
